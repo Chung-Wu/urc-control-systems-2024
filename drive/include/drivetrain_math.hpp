c16471faa189e200d726b6bb1b1ef1decc9da638
MichaelYKersey <144640226+MichaelYKersey@users.noreply.github.com>
2025-09-06 13:55:13 -0700

    get outline of module and add comments to drive_math

--- drive/include/drivetrain_math.hpp
+++ drive/include/drivetrain_math.hpp
@@ -17,29 +17,22 @@ std::array<vector2d, module_count> chassis_velocities_to_module_vectors(
 
 // module state validity score (0 is complete match)
 float module_validity_strain_score(
-  std::array<swerve_module, module_count> p_modules,
+  std::array<swerve_module, module_count>& p_modules,
   std::array<vector2d, module_count> p_vectors);
-// {
-//     vector2d transition(0,0);
-//     for (auto& v : p_vectors) {
-//         transition =  transition-(v/p_vectors.size());
-//     }
-
-//     return 0.0;
-// }
 
 // calculate freest state
-swerve_module_state calculate_freest_state(swerve_module p_module,
+swerve_module_state calculate_freest_state(swerve_module& p_module,
                                            vector2d p_target_vector);
 // calc closest equivalent
-swerve_module_state calculate_closest_state(swerve_module p_module,
+swerve_module_state calculate_closest_state(swerve_module& p_module,
                                             vector2d p_target_vector);
-// calc interpolation time (single module and/or all)
-sec calculate_total_interpolation_time(swerve_module p_module,
+// calc interpolation time for a single module
+sec calculate_total_interpolation_time(swerve_module& p_module,
                                        swerve_module_state p_end_state);
 
+//calculates total interpolation time
 sec calculate_total_interpolation_time(
-  std::array<swerve_module, module_count> p_modules,
+  std::array<swerve_module, module_count>& p_modules,
   std::array<swerve_module_state, module_count> p_end_states);
 
 // detect interpolation conflicts (ivalid interpolation how calc?)
@@ -49,10 +42,12 @@ std::array<swerve_module_state, module_count> scale_down_propulsion_speed(
   std::array<swerve_module, module_count> p_modules,
   std::array<swerve_module_state, module_count> p_states);
 
+//calculate the target state for interpolation mid interpolation
 swerve_module_state interpolate_state(float p_portion,
                                       swerve_module_state p_start_state,
                                       swerve_module_state p_end_state);
 
+//get states to set set mid interpolation
 std::array<swerve_module_state, module_count> interpolate_states(
   sec p_cycle_time,
   std::array<swerve_module, module_count> p_modules,
--- drive/include/swerve_module.hpp
+++ drive/include/swerve_module.hpp
@@ -15,12 +15,16 @@ struct swerve_module_settings
   deg_per_sec turn_speed;
   hal::degrees min_angle;
   hal::degrees max_angle;
+  hal::degrees position_tolerance;
+  meters_per_sec velocity_tolerance;
+  sec tolerance_timeout;
 };
 
 class swerve_module
 {
 public:
   swerve_module_settings settings;
+
   // vars:
   // steer postion read & control (maybe also contains restrictions) (has homing
   // function) prop vel read & control (maybe also contrans restrictions)
@@ -29,29 +33,20 @@ public:
 
   // methods:
   // calc target state for vector (closest angle for valid state)
-  vector2d calc_module_vector(chassis_velocities p_chassis_velocities);
   // set target state
   void set_target_state(swerve_module_state p_target_state);
-  // TODO: move to calc file?
-  swerve_module_state calc_closest_state(vector2d p_module_vector);
-  // optimize freedom
-  swerve_module_state calc_freest_state(vector2d p_module_vector);
   bool can_reach_state(swerve_module_state p_state);
-
-  // time for transition
-  sec transition_time(swerve_module_state p_end_state);
 
   // reads state cache
   swerve_module_state get_actual_state_cache();
   // refresh state cache
   swerve_module_state refresh_actual_state_cache();
 
-  // get actual & target state
-  // get actual angle
-  // get actual prop vel
-  // get target angle
-  // get target prop vel
-  // set target angle
-  // set target prop vel
+  //get target state
+  swerve_module_state get_target_state();
+  //update tolorance debouncer
+  void update_tolerance_debouncer();
+  //tolorance timeout
+  bool tolerance_timed_out();
 };
 }  // namespace sjsu::drive
